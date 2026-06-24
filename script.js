d0634b5353747b9e2aca6c169f208b51ea700799
hedynzon <edinson.raymundo@gmail.com>
2026-06-24 18:47:11 -0500

    feat & feat: cumpliendo retos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const inputIntento = document.getElementById("inputIntento");
 const btnAdivinar = document.getElementById("btnAdivinar");
 const mensaje = document.getElementById("mensaje");
 const contador = document.getElementById("contador");
+const mejorpuntaje = document.getElementById("mejorpuntaje");
 const historial = document.getElementById("historial");
 const btnReiniciar = document.getElementById("btnReiniciar");
 const tarjeta = document.getElementById("game-card");
@@ -23,6 +24,7 @@ mostrarMensaje("Bienvenido al juego", "#e94560");
 
 let numeroSecreto = Math.floor(Math.random() * 100) + 1;
 let intentos = 0;
+let mejorPuntaje = 0;
 let historialIntentos = [];
 
 console.log("(DEBUG)Número secreto:", numeroSecreto);
@@ -35,26 +37,41 @@ function verificarIntento() {
     }
 
     intentos++;
-    contador.textContent = "Intentos: " + intentos;
+    if (intentos <= 10) {
+        contador.textContent = "Intentos: " + intentos;
+    /*historialIntentos.push(valor);
+    historial.textContent = "Historial: " + historialIntentos.join(", ");*/
+        
+        let color = valor > numeroSecreto ? '#ff6b6b' : valor < numeroSecreto ? '#4ecdc4' : '#00ff88';
+        historial.innerHTML += '<span class="guess-pill" style="background:' + color + '30; color:' + color + '">' + valor + '</span>';
 
-    historialIntentos.push(valor);
-    historial.textContent = "Historial: " + historialIntentos.join(", ");
-    if(valor === numeroSecreto) {
-        mostrarMensaje("🎉¡Felicidades! Has adivinado el número secreto.", "green");
+        if(valor === numeroSecreto) {
+            mostrarMensaje("🎉¡Felicidades! Has adivinado el número secreto.", "green");
+            if (mejorPuntaje===0 || mejorPuntaje > intentos) {
+                mejorpuntaje.textContent = "Mejor Puntaje: " + intentos;
+                mejorPuntaje = intentos;
+            }            
+            inputIntento.disabled = true;
+            btnAdivinar.disabled = true;
+            btnReiniciar.style.display = "inline-block";
+            tarjeta.style.borderColor = "green";
+            tarjeta.style.boxShadow = "0 0 40px rgba(0,255,136,0.3)";
+        }
+        else if(valor > numeroSecreto) {
+                mostrarMensaje("📈 Muy alto, intenta más bajo", "#ff6b6b");
+                mostrarMensaje(obtenerPista(valor, numeroSecreto), "#ff6b6b");
+            } else {
+                    mostrarMensaje("📉Muy bajo, intenta más alto", "#4ecdc4");                mostrarMensaje(obtenerPista(valor, numeroSecreto), "#4ecdc4");  
+            }
+            inputIntento.value = "";
+            inputIntentofocus();
+    } else {
+        mostrarMensaje("💀 Has agotado tus 10 intentos. El número secreto era: " + numeroSecreto, "red");
         btnAdivinar.disabled = true;
         btnReiniciar.style.display = "inline-block";
-        tarjeta.style.borderColor = "green";
-        tarjeta.style.boxShadow = "0 0 40px rgba(0,255,136,0.3)";
+        tarjeta.style.borderColor = "red";
+        tarjeta.style.boxShadow = "0 0 40px rgba(255,0,0,0.3)";
     }
-    else if(valor > numeroSecreto) {
-            mostrarMensaje("📈 Muy alto, intenta más bajo", "#ff6b6b");
-            mostrarMensaje(obtenerPista(valor, numeroSecreto), "#ff6b6b");
-         } else {
-            mostrarMensaje("📉Muy bajo, intenta más alto", "#4ecdc4");
-            mostrarMensaje(obtenerPista(valor, numeroSecreto), "#4ecdc4");  
-        }
-        inputIntento.value = "";
-        inputIntento.focus();
 }
 
 function reiniciarJuego() {
@@ -62,8 +79,12 @@ function reiniciarJuego() {
     intentos = 0;
     historialIntentos = [];
     contador.textContent = "Intentos: 0";
-    historial.textContent = "Historial: ";
+    /*historial.textContent = "Historial: ";*/
+
+    historial.innerHTML = '';
+
     mostrarMensaje("Juego reiniciado. Intenta adivinar el número secreto.", "#e94560");
+    inputIntento.disabled = false;
     btnAdivinar.disabled = false;
     btnReiniciar.style.display = "none";
     inputIntento.value = "";
@@ -88,6 +109,7 @@ function obtenerPista(intento, secreto) {
     return '❄️ Frío';
   }
 }
+
 
 btnAdivinar.addEventListener("click", verificarIntento); 
 btnReiniciar.addEventListener("click", reiniciarJuego);
